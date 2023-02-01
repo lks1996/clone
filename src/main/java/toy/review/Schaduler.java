bd3c31e8ad70856522807cdf69b36bf36ae36404
lks1996 <mosplee@naver.com>
2023-02-01 19:46:27 +0900

    update Scheduler'

--- src/main/java/toy/review/Schaduler.java
+++ src/main/java/toy/review/Schaduler.java
@@ -10,6 +10,26 @@ public class Schaduler {
     @Scheduled(cron = "10 * * * * *")
     public void printDate () {
         System.out.println(new Date().toString());
+
+        try {
+            // Run script
+            Process process = Runtime.getRuntime().exec("/home/ubuntu/app/git/deploy.sh");
+
+//            // Read output
+//            StringBuilder output = new StringBuilder();
+//            BufferedReader reader = new BufferedReader(
+//                    new InputStreamReader(process.getInputStream()));
+//
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                output.append(line);
+//            }
+//
+//            System.out.println(output.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
